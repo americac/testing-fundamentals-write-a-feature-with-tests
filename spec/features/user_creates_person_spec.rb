0030f0e2d8f0d1c62057ccb29729ab8a9fc1aef3
America Calleros <america.quintero@gmail.com>
2016-06-30 15:05:47 -0500

    Add tests to edit person

--- spec/features/user_creates_person_spec.rb
+++ spec/features/user_creates_person_spec.rb
@@ -3,15 +3,20 @@ require "rails_helper"
 feature "user creates person" do
   scenario "with valid data" do
     visit new_person_path
-    fill_in "person_first_name", with: "Bob"
+    fill_in "person_first_name", with: "Coach"
     click_button "Create Person"
 
     expect(page).to have_content("Person created")
   end
 end
 
+
 feature "user edits existing person" do
   scenario "with valid data" do
-    pending "write your spec here"
+    person = Person.create(:first_name => 'Leo')
+    visit ('/people/' + person.id.to_s)
+    click_link "Edit"
+    expect(page).to have_content("Edit "+ person.first_name)
+    find_button('Update Person').visible?
   end
 end
